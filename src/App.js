fb7d224c33c96d4ebe7dc094d9b57148bf158e8b
taleore <taleore@gmail.com>
2019-09-23 16:10:57 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.css";
+
+class App extends from React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  conponentDidMount() {
+    this.getCharacter();
+  }
+
+  getCharacter() {
+    
+  }
+
+}
+
+
+
+export default App;
