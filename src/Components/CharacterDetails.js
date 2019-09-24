49b3a7f19d3e59155e3483e6127505edcdb9ba90
taleore <taleore@gmail.com>
2019-09-24 12:23:23 +0200

    Fix details

--- src/Components/CharacterDetails.js
+++ src/Components/CharacterDetails.js
@@ -6,24 +6,35 @@ const CharacterDetails = props => {
   const id = parseInt(routerProps.match.params.id);
 
   const character = characters.filter(item => item.id === id);
-  const { name, image, status, species, origin, episode } = character[0];
 
-  return (
-    <React.Fragment>
-      <div className="character_detail">
-        <div className="character_detail__img">
-          <img src={image} alt={name} />
+  if (character[0]) {
+    const { name, image, status, species, origin, episode } = character[0];
+
+    return (
+      <React.Fragment>
+        <Link to="../">Volver</Link>
+        <div className="character_detail">
+          <div className="character_detail__img">
+            <img src={image} alt={name} />
+          </div>
+          <div className="character_detail__container">
+            <h2 className="character_detail__name">{name}</h2>
+            <h3>status: {status}</h3>
+            <h3>species: {species}</h3>
+            <h3>origin: {origin.name}</h3>
+            <h3>episodes: {episode.length}</h3>
+          </div>
         </div>
-        <h2 className="character_detail__name">{name}</h2>
-        <h3>{status}</h3>
-        <h3>{species}</h3>
-        <h3>{origin.name}</h3>
-        <h3>{episode.length}</h3>
-      </div>
-
-      <Link to="../">Volver al listado</Link>
-    </React.Fragment>
-  );
+      </React.Fragment>
+    );
+  } else {
+    return (
+      <React.Fragment>
+        <p>noooooooo</p>
+        <Link to="../">Volver</Link>
+      </React.Fragment>
+    );
+  }
 };
 
 export default CharacterDetails;
